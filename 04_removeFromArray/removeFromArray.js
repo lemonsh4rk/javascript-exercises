0c97485820e4bc72373378437db5139a3cf8a88c
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-12 20:38:45 -0300

    feat: add solution to 04 exercise
    I'm still having problem with these, at least i'm understanding the logic i guess :c

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...values) {
+  let newArray = [];
+  array.forEach((item) => {
+    if (!values.includes(item)) {
+      newArray.push(item);
+    }
+  })
+  return newArray
+}
 // Do not edit below this line
 module.exports = removeFromArray;
