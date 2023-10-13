225207ca679c3aeee66470a10d48bd961521162e
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-12 23:27:08 -0300

    feat: add solution to 07 excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = (temperature - 32) * 5 / 9;
+  return Number(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = temperature * 9 / 5 + 32;
+  return Number(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
