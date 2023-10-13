6d39deacbe7ba56c78d1346e2133be07459b51ec
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-12 21:21:10 -0300

    feat: add solution to 06 excercise
    FINALLY i did one all by myself, jesus christ ;-;

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// If the last two digits of a number are divisible by 4, the number is divisible by 4 --- IIIIIRrelevant on java lmao, you just need the remainder to be 0, so number % 4 === 0
+// include any year that is divisible by 400 so, number % 400 === 0
+// also exclude any year that is divisible by 100. so skip any number % 100 === 0 
+// 
+
+if (year % 400 === 0) {
+  return true;
+} else if (year % 100 === 0) {
+  return false;
+} else if (year % 4 === 0){
+  return true;
+} else {
+  return false;
+}
+
+
 
 };
 
