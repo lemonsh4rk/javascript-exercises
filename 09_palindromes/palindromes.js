329cd224dc72e9b91405ec72beca7ccfaf444fc0
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-30 18:52:48 -0300

    feat: add solution to 09 excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  let re = /[^A-Za-z0-9]/g;
+  let lowercasePhrase = phrase.toLowerCase().replace(re, '');
+  let reversePhrase = lowercasePhrase.split('').reverse().join('');
+
+  return reversePhrase === lowercasePhrase;
 
 };
 
