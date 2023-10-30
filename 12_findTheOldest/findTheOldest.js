bca59d8254f4cdca1f92456a6f0eace20214fdfd
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-30 20:07:34 -0300

    feat: add solution to 12 excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let currentYear = 2023;
+  let newArray = array.sort(function(a, b) {
+    if (!a.yearOfDeath) {
+      let personA = a.yearOfBirth - currentYear;
+      let personB = b.yearOfBirth - b.yearOfDeath;
+      return personA > personB ? -1 : 1;
+    } else if (!b.yearOfDeath) {
+      let personA = a.yearOfBirth - a.yearOfDeath;
+      let personB = b.yearOfBirth - currentYear;
+      return personA > personB ? -1 : 1;
+    } else {
+      let personA = a.yearOfBirth - a.yearOfDeath;
+    let personB = b.yearOfBirth - b.yearOfDeath;
+    return personA > personB ? -1 : 1;
+    }
+  })
 
+  let oldestArray = newArray.slice(-1);
+  return oldestArray[0];
 };
 
 // Do not edit below this line
