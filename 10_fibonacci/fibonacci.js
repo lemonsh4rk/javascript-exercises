e65fcc0f9d51e41b461a7851c650b66cbc5aebaf
lemonshark <83254974+lemonsh4rk@users.noreply.github.com>
2023-10-30 19:17:34 -0300

    feat: add solution to 10 excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (number < 0) {
+    return 'OOPS'
+  }
 
+
+  let firstDigit = 0;
+  let secondDigit = 1;
+  let nextSum;
+  let fibonacciArray = [1];
+
+  for (let i = 0; i < 25; i++) {
+    nextSum = firstDigit + secondDigit;
+    fibonacciArray.push(nextSum);
+
+    firstDigit = secondDigit;
+    secondDigit = nextSum;
+  }
+
+  return fibonacciArray[number - 1];
 };
 
 // Do not edit below this line
